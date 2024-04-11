0757180d8df7b7bbf4ab3176c4235d4dfb8fb862
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-04-11 16:48:26 +0530

    fix

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,7 +4,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: '/resume', title: 'Resume' },
+  // { href: '/resume', title: 'Resume' },
 ]
 
 export default headerNavLinks
